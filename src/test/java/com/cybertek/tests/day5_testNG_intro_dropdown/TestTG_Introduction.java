66cd683ef8e7a25a62d3c7f0ad9bb54ab1cf72d6
Mayasa79 <mayasa.sabie@gmail.com>
2021-01-08 14:38:30 -0600

    hello

---
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdown/TestTG_Introduction.java
@@ -0,0 +1,44 @@
+package com.cybertek.tests.day5_testNG_intro_dropdown;
+
+import org.testng.annotations.*;
+
+public class TestTG_Introduction {
+
+    @BeforeMethod
+    public void setupMethod() {
+        System.out.println("Before method is running");
+    }
+
+    @AfterMethod
+    public void tearDownMethod() {
+        System.out.println("After method is running ...");
+    }
+
+    /*
+    @BeforeClass will make the method run once before everything else in the class.
+     */
+    @BeforeClass
+    public void setupClass() {
+        System.out.println("Before class is running...");
+    }
+
+    /*
+     @AfterClass will make the method run once after everything else in the class.
+      */
+    @AfterClass
+    public void teardownClass() {
+        System.out.println("After class is running...");
+    }
+
+
+    @Test
+    public void test1() {
+        System.out.println("Running test 1...");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Running test 2...");
+    }
+
+}
